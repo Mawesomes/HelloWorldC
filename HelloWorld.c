ac2114f210efa5204d8d8dfa3460308373a62f79
Mawesomes <38977532+Mawesomes@users.noreply.github.com>
2022-01-14 11:51:21 -0500

    Complete project
    Code and .exe file functional

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+//(One line commment)
+// #include is used to import tools used/required in the program
+#include<stdio.h>
+
+
+/*(Multy-line comment)*/
+
+
+int main(){
+    // Function to print values and Strings in the terminal 
+    printf("Hello World");
+    // End of function with exit code (0)
+    return 0;
+    
+}
